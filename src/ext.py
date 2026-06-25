71689e1b1c1f240f63e22e4c5f7458f8f794f7fd
Shuhang Xue <xueshuhang@MacBook-Pro-748.local>
2026-06-25 15:55:11 -0400

    Update ext.py

--- src/ext.py
+++ src/ext.py
@@ -7,9 +7,16 @@ In addition, it should also provide methods to assert and to retrieve
 known differentials that will be used to bootstrap the SAT solver.
 """
 
+import csv
+
 from .ext_class import ExtClass
 from .differential import Differential
-from .find_differential import get_classes  
+from .find_differential import get_classes()  
+"""from some document import rho-periodic_list, which is a list of rho-periodic elements"""
+"""from some document import product_dict, which stores [element1, element2, their product]"""
+
+
+classes= get_classes()
 
 class Ext:
     """
@@ -33,7 +40,11 @@ class Ext:
         Returns:
             list[ExtClass]: A list of all the ExtClasses whose coweight is less than the given maximum.
         """ 
-        pass
+        classes_up_to_coweight = []
+        for element in classes:
+            if element['stem'] - element['weight'] <= coweight:
+                classes_up_to_coweight.append(ExtClass(element['name'], (element['stem'], element['Adams filtration'], element['weight'])))
+        return classes_up_to_coweight
 
     def get_classes_in_fixed_degree(self, N: int) -> list[ExtClass]:
         """
@@ -45,7 +56,11 @@ class Ext:
         Returns:
             list[ExtClass]: A list of all the ExtClasses whose fixed degree is equal to N.
         """
-        pass
+        classes_with_N=[]
+        for element in classes:
+            if element['stem'] + element['Adams filtration'] - element['weight'] == N:
+                classes_with_N.append(ExtClass(element['name'], (element['stem'], element['Adams filtration'], element['weight'])))
+        return classes_with_N
 
     def get_classes_in_tridegree(self, tridegree: tuple[int, int, int]) -> list[ExtClass]:
         """
@@ -57,7 +72,11 @@ class Ext:
         Returns:
             list[ExtClass]: A list of all the ExtClasses in the given tridegree.
         """
-        pass
+        classes_in_tridegree = []
+        for element in classes:
+            if (element['stem'], element['Adams filtration'], element['weight']) == tridegree:
+                classes_in_tridegree.append(ExtClass(element['name'], (element['stem'], element['Adams filtration'], element['weight'])))
+        return classes_in_tridegree
 
     def get_rho_periodic_elements(self) -> list[ExtClass]:
         """
@@ -69,7 +88,11 @@ class Ext:
         Returns:
             list[ExtClass]: A list of all the ExtClasses that are known to be rho periodic.
         """
-        pass
+        classes_rho_periodic=[]
+        for element in classes:
+            if element['stem']+element['Adam filtration']-2*element['weight'] == 0:
+                classes_rho_periodic.append(ExtClass(element['name'], (element['stem'], element['Adams filtration'], element['weight'])))
+        return classes_rho_periodic
 
     def is_rho_periodic(self, ext_class: ExtClass) -> bool:
         """
@@ -81,7 +104,10 @@ class Ext:
         Returns:
             bool: True if the ExtClass is known to be rho periodic, False otherwise.
         """
-        pass
+        if ext_class.degree[0] + ext_class.degree[1] - 2 * ext_class.degree[2] == 0:
+            return True
+        otherwise:
+            return False
 
     def get_h1_periodic_elements(self) -> list[ExtClass]:
         """
@@ -91,7 +117,12 @@ class Ext:
         Returns:
             list[ExtClass]: A list of all the ExtClasses that are known to be h1 periodic.
         """
-        pass
+        classes_h1_periodic=[]
+        for element in classes:
+            for rho_periodic_element in rho_periodic_list:
+                if element.name == rho_periodic_element:
+                    classes_h1_periodic.append(ExtClass(element['name'], (element['stem'], element['Adams filtration'], element['weight'])))
+        return classes_h1_periodic
 
     def is_h1_periodic(self, ext_class: ExtClass) -> bool:
         """
@@ -103,7 +134,10 @@ class Ext:
         Returns:
             bool: True if the ExtClass is known to be h1 periodic, False otherwise.
         """
-        pass
+        for rho_periodic_element in rho_periodic_list:
+            if ext_class.name == rho_periodic_element:
+                return True
+        return False
 
     def get_known_differentials(self) -> list[Differential]:
         """
@@ -112,7 +146,7 @@ class Ext:
         Returns:
             list[Differential]: A list of all the known differentials in the rho-Bockstein spectral sequence.
         """
-        pass
+        return differentials
 
     def add_known_differential(self, differential: Differential) -> None:
         """
@@ -121,5 +155,5 @@ class Ext:
         Args:
             differential (Differential): The Differential to add to the list of known differentials.
         """
-        pass
+        return self.differentials().append(differential)
 
